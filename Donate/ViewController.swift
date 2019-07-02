4cf8615f014fd88f2f9e62808cfd2db36e27107a
G. Moraleda <guillermo.moraleda@cluno.com>
2019-07-02 13:43:24 +0200

    Initial commit

---
+++ Donate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Donate
+//
+//  Created by Guillermo Moraleda on 02.07.19.
+//  Copyright © 2019 Guillermo Moraleda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
